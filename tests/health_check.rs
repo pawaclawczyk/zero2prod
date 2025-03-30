d4110a2a4d87edf62f52773fdd0c08b54c510fc6
Paweł Wacławczyk <p.a.waclawczyk@gmail.com>
2025-03-30 16:09:05 +0200

    Add dummy subscribe endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -15,6 +15,50 @@ async fn test_health_check() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .post(format!("{}/subscribe", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body("email=john%40example.com&name=John%20Doe")
+        .send()
+        .await
+        .expect("Failed to send request.");
+
+    assert!(response.status().is_success());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("", "missing both email and name"),
+        ("name=John%20Doe", "missing email"),
+        ("email=john%40example.com", "missing name"),
+    ];
+
+    for (data, expected_error) in test_cases {
+        let response = client
+            .post(format!("{}/subscribe", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(data)
+            .send()
+            .await
+            .expect("Failed to send request.");
+
+        assert_eq!(
+            response.status(),
+            400,
+            "The API didn't fail with 400 Bad Request when payload was {}",
+            expected_error
+        );
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port.");
     let port = listener.local_addr().unwrap().port();
